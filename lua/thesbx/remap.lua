01d462598a64698d818e68087509e1c522db92cc
Matt Miller <mattmillerf@pm.me>
2022-12-16 10:05:48 -0500

    updates

--- lua/thesbx/remap.lua
+++ lua/thesbx/remap.lua
@@ -1,7 +1,11 @@
 local nnoremap = require("thesbx.keymap").nnoremap
+local builtin = require("telescope.builtin")
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
 nnoremap("<leader>pt", "<cmd>Telescope find_files<CR>")
+nnoremap("<leader>ps", function()
+    builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
 nnoremap("<leader>gs", "<cmd>:Git<CR>")
 nnoremap("<leader>gb", "<cmd>:G blame<CR>")
 nnoremap("<leader>ga", "<cmd>Git fetch --all<CR>")
