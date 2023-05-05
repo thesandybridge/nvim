19b2a81c266fbdad9606fbe90a50bd9ba2f14b7c
Matt Miller <mattmillerf@pm.me>
2023-05-04 22:04:11 -0400

    rm highlight

--- lua/thesbx/find_replace.lua
+++ lua/thesbx/find_replace.lua
@@ -1,7 +1,3 @@
-vim.api.nvim_exec([[
-  highlight CustomHighlightGroup guibg=Yellow guifg=Black
-]], false)
-
 vim.api.nvim_create_user_command("FindReplace", function(opts)
     if #opts.fargs < 1 then
         vim.api.nvim_err_writeln("FindReplace requires a search_pattern argument")
@@ -10,9 +6,6 @@ vim.api.nvim_create_user_command("FindReplace", function(opts)
 
     local search_pattern = vim.fn.escape(opts.fargs[1], '\\/.*$^~[]')
     local replace_escaped = vim.fn.escape(opts.fargs[2], '\\/&~')
-
-    local highlight= string.format("match CustomHighlightGroup /%s/", search_pattern)
-    vim.cmd(highlight)
 
     local command = string.format("%%s/%s/%s/g", search_pattern, replace_escaped)
     vim.cmd(command)
