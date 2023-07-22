df7e2f3ce34e95e2108eba2925359f2e1398a61f
Matt Miller <mattmillerf@pm.me>
2023-07-22 14:53:37 -0400

    neovim updates

--- lua/thesbx/packer.lua
+++ lua/thesbx/packer.lua
@@ -10,6 +10,7 @@ return require('packer').startup(function(use)
     use 'navarasu/onedark.nvim'
     use 'folke/tokyonight.nvim'
     use 'petertriho/nvim-scrollbar'
+    use 'github/copilot.vim'
     use {
         'nvim-telescope/telescope.nvim', tag = '0.1.0',
         requires = { {'nvim-lua/plenary.nvim'} }
--- plugin/packer_compiled.lua
+++ plugin/packer_compiled.lua
@@ -104,6 +104,11 @@ _G.packer_plugins = {
     path = "/home/sbx/.local/share/nvim/site/pack/packer/start/cmp_luasnip",
     url = "https://github.com/saadparwaiz1/cmp_luasnip"
   },
+  ["copilot.vim"] = {
+    loaded = true,
+    path = "/home/sbx/.local/share/nvim/site/pack/packer/start/copilot.vim",
+    url = "https://github.com/github/copilot.vim"
+  },
   ["emmet-vim"] = {
     loaded = true,
     path = "/home/sbx/.local/share/nvim/site/pack/packer/start/emmet-vim",
