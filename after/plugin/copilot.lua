ac330e4a866716fc7fd5e58efbcf3bd05efbc3ad
Matt Miller <mattmillerf@pm.me>
2023-11-29 09:21:04 -0500

    fix bug

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,2 +1,5 @@
-vim.api.nvim_set_keymap('i', '<C-J>', '<Plug>(copilot-i-<Tab>)', { silent = true })
+vim.keymap.set('i', '<C-J>', 'copilot#Accept("\n")', {
+    expr = true,
+    replace_keycodes = false
+})
 vim.g.copilot_no_tab_map = true
