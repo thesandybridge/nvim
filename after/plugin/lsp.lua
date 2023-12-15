9720c0e0dd1a846803ae41a89227793c8e5d511c
Matt Miller <mattmillerf@pm.me>
2023-12-15 10:49:13 -0500

    fix errors

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,15 +5,11 @@ require('mason-lspconfig').setup({
   ensure_installed = {
       'tsserver',
       'rust_analyzer',
-      'eslilnt',
+      'eslint',
       'intelephense'
   },
   handlers = {
       lsp.default_setup,
-      lua_ls = function()
-          local lua_opts = lsp.nvim_lua_ls()
-          require('lspconfig').lua_ls.setup(lua_opts)
-      end,
   }
 })
 
