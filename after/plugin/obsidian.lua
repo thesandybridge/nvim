a7399bbc054fa53cb3500cace927de26723bdc1c
Matt Miller <mattmillerf@pm.me>
2024-03-02 12:48:20 -0500

    obsidian config

--- after/plugin/obsidian.lua
+++ after/plugin/obsidian.lua
@@ -27,5 +27,11 @@ require("obsidian").setup({
             opts = { buffer = true },
         },
     },
+    attachments = {
+        -- The default folder to place images in via `:ObsidianPasteImg`.
+        -- If this is a relative path it will be interpreted as relative to the vault root.
+        -- You can always override this per image by passing a full path to the command instead of just a filename.
+        img_folder = "assets/images",  -- This is the default
+    },
 
 })
