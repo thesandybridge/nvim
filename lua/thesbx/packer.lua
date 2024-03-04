ef46472c74f1b850700514df93e3cffd727f8e35
Matt Miller <mattmillerf@pm.me>
2024-03-03 22:21:30 -0500

    edits

--- lua/thesbx/packer.lua
+++ lua/thesbx/packer.lua
@@ -1,111 +1,106 @@
-local function is_firenvim()
-    return vim.g.started_by_firenvim ~= nil
-end
+--This file can be loaded by calling `lua require('plugins')` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-    if not is_firenvim() then
-        -- Packer can manage itself
-        use 'wbthomason/packer.nvim'
-        use 'othree/html5.vim'
-        use 'pangloss/vim-javascript'
-        use 'leafgarland/typescript-vim'
-        use 'maxmellon/vim-jsx-pretty'
-        use 'othree/javascript-libraries-syntax.vim'
-        use 'navarasu/onedark.nvim'
-        -- use 'folke/tokyonight.nvim'
-        use { "catppuccin/nvim", as = "catppuccin" }
-        use { "catppuccin/vim", as = "catppuccin_vim" }
-        use 'petertriho/nvim-scrollbar'
-        use 'github/copilot.vim'
-        use 'lewis6991/gitsigns.nvim'
-        use { 'rcarriga/nvim-dap-ui', requires = {'mfussenegger/nvim-dap'} }
-        use 'theHamsta/nvim-dap-virtual-text'
-        use 'leoluz/nvim-dap-go'
-        use {
-            'nvim-telescope/telescope.nvim',
-            requires = { {'nvim-lua/plenary.nvim'} }
-        }
-        use {'nvim-telescope/telescope-ui-select.nvim' }
-        use {
-            'NeogitOrg/neogit',
-            dependencies = {
-                "nvim-lua/plenary.nvim",
-                "nvim-telescope/telescope.nvim",
-                "sindrets/diffview.nvim",
-            },
-        }
-        use {
-            'nvim-lualine/lualine.nvim',
-            requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-        }
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use 'othree/html5.vim'
+    use 'pangloss/vim-javascript'
+    use 'leafgarland/typescript-vim'
+    use 'maxmellon/vim-jsx-pretty'
+    use 'othree/javascript-libraries-syntax.vim'
+    use 'navarasu/onedark.nvim'
+    -- use 'folke/tokyonight.nvim'
+    use { "catppuccin/nvim", as = "catppuccin" }
+    use { "catppuccin/vim", as = "catppuccin_vim" }
+    use 'petertriho/nvim-scrollbar'
+    use 'github/copilot.vim'
+    use 'lewis6991/gitsigns.nvim'
+    use { 'rcarriga/nvim-dap-ui', requires = {'mfussenegger/nvim-dap'} }
+    use 'theHamsta/nvim-dap-virtual-text'
+    use 'leoluz/nvim-dap-go'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    use {'nvim-telescope/telescope-ui-select.nvim' }
+    use {
+        'NeogitOrg/neogit',
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "sindrets/diffview.nvim",
+        },
+    }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
 
-        use 'norcalli/nvim-colorizer.lua'
-        use 'mattn/emmet-vim'
-        use {
-            'kkoomen/vim-doge',
-            run = ':call doge#install()'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'mattn/emmet-vim'
+    use {
+        'kkoomen/vim-doge',
+        run = ':call doge#install()'
+    }
+    use({
+        "folke/trouble.nvim",
+    })
+    use {
+        "luckasRanarison/nvim-devdocs",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "nvim-treesitter/nvim-treesitter",
         }
-        use({
-            "folke/trouble.nvim",
-        })
-        use {
-            "luckasRanarison/nvim-devdocs",
-            requires = {
-                "nvim-lua/plenary.nvim",
-                "nvim-telescope/telescope.nvim",
-                "nvim-treesitter/nvim-treesitter",
-            }
-        }
-        use {
-            "folke/todo-comments.nvim",
-            requires = "nvim-lua/plenary.nvim",
-        }
-        use 'tpope/vim-fugitive'
-        use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-        use 'nvim-treesitter/playground'
-        use 'nvim-treesitter/nvim-treesitter-context'
-        use 'hrsh7th/vim-vsnip'
-        use 'hrsh7th/vim-vsnip-integ'
-        use "akinsho/toggleterm.nvim"
-        use "theprimeagen/harpoon"
-        use "lukas-reineke/indent-blankline.nvim"
-        use 'wakatime/vim-wakatime'
-        use 'wfxr/minimap.vim'
-        use {
-            'VonHeikemen/lsp-zero.nvim',
-            requires = {
-                -- LSP Support
-                {'neovim/nvim-lspconfig'},
-                {'williamboman/mason.nvim'},
-                {'williamboman/mason-lspconfig.nvim'},
+    }
+    use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+    }
+    use 'tpope/vim-fugitive'
+    use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+    use 'nvim-treesitter/playground'
+    use 'nvim-treesitter/nvim-treesitter-context'
+    use 'hrsh7th/vim-vsnip'
+    use 'hrsh7th/vim-vsnip-integ'
+    use "akinsho/toggleterm.nvim"
+    use "theprimeagen/harpoon"
+    use "lukas-reineke/indent-blankline.nvim"
+    use 'wakatime/vim-wakatime'
+    use 'wfxr/minimap.vim'
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        requires = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-                -- Autocompletion
-                {'hrsh7th/nvim-cmp'},
-                {'hrsh7th/cmp-buffer'},
-                {'hrsh7th/cmp-path'},
-                {'saadparwaiz1/cmp_luasnip'},
-                {'hrsh7th/cmp-nvim-lsp'},
-                {'hrsh7th/cmp-nvim-lua'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'saadparwaiz1/cmp_luasnip'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'hrsh7th/cmp-nvim-lua'},
 
-                -- Snippets
-                {'L3MON4D3/LuaSnip'},
-                {'rafamadriz/friendly-snippets'},
-            },
-            branch = 'v3.x'
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
+        },
+        branch = 'v3.x'
+    }
+    use "michaelrommel/nvim-silicon"
+    use "mvllow/modes.nvim"
+    use({
+        "epwalsh/obsidian.nvim",
+        tag = "*",
+        requires = {
+            "nvim-lua/plenary.nvim",
+
         }
-        use "michaelrommel/nvim-silicon"
-        use "mvllow/modes.nvim"
-        use({
-            "epwalsh/obsidian.nvim",
-            tag = "*",
-            requires = {
-                "nvim-lua/plenary.nvim",
-
-            }
-        })
-    end
-    use "glacambre/firenvim"
+    })
 end)
