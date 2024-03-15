644fd0bec861ba260570df35f02d5eed534d8736
Matt Miller <mattmillerf@pm.me>
2024-03-15 15:49:55 -0400

    changed git signs

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,11 +1,11 @@
 require('gitsigns').setup {
     signs = {
-        add          = { text = '│' },
-        change       = { text = '│' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
         changedelete = { text = '~' },
-        untracked    = { text = '┆' },
+        untracked = { text = '?' }
     },
     signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
     numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
