a54fbe2d1ea36bfc9caa80a10c6bc8959507b619
Matt Miller <mattmillerf@pm.me>
2024-03-15 16:47:20 -0400

    install blame, remove fugitive

--- lua/thesbx/packer.lua
+++ lua/thesbx/packer.lua
@@ -27,6 +27,7 @@ return require('packer').startup(function(use)
     }
     use {'nvim-telescope/telescope-ui-select.nvim' }
     use "sindrets/diffview.nvim"
+    use "FabijanZulj/blame.nvim"
     use {
         'NeogitOrg/neogit',
         dependencies = {
@@ -61,7 +62,7 @@ return require('packer').startup(function(use)
         "folke/todo-comments.nvim",
         requires = "nvim-lua/plenary.nvim",
     }
-    use 'tpope/vim-fugitive'
+    --use 'tpope/vim-fugitive'
     use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
     use 'nvim-treesitter/playground'
     use 'nvim-treesitter/nvim-treesitter-context'
