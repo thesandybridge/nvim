9ae3e356afe51cf3209ace595dc723f504298c7e
Matt Miller <mattmillerf@pm.me>
2024-04-05 12:31:45 -0400

    new updates

--- lua/thesbx/packer.lua
+++ lua/thesbx/packer.lua
@@ -28,6 +28,7 @@ return require('packer').startup(function(use)
     use {'nvim-telescope/telescope-ui-select.nvim' }
     -- GIT --
     use "sindrets/diffview.nvim"
+    --use "thesandybridge/blame.nvim"
     use "FabijanZulj/blame.nvim"
     use {
         'NeogitOrg/neogit',
