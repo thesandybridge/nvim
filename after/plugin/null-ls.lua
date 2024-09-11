4665eb0e424311b97c1fcc445e570e05aede6f9f
Matt Miller <mattmillerf@pm.me>
2024-09-10 23:35:19 -0400

    edits

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -2,11 +2,6 @@ local null_ls = require("null-ls")
 
 null_ls.setup({
     sources = {
-        -- Enable ESLint formatting
-        null_ls.builtins.formatting.prettierd.with({
-            condition = function(utils)
-                return utils.root_has_file({ ".eslintrc", ".eslintrc.js", ".eslintrc.json" }) -- Only enable if an ESLint config exists in the root
-            end,
-        }),
+        null_ls.builtins.formatting.prettierd,
     },
 })
