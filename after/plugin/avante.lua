2d2b713c3f33178c6c7a8001db5b1a3b95b4bbdd
Matt Miller <mattmillerf@pm.me>
2025-02-10 18:42:46 -0500

    avante support

--- after/plugin/avante.lua
+++ after/plugin/avante.lua
@@ -1,2 +1,17 @@
 require("avante_lib").load()
-require("avante").setup()
+require("avante").setup(
+    {
+        ---@alias Provider "claude" | "openai" | "azure" | "gemini" | "cohere" | "copilot" | string
+        provider = "claude", -- Recommend using Claude
+        -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
+        -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
+        -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
+        auto_suggestions_provider = "claude",
+        claude = {
+            endpoint = "https://api.anthropic.com",
+            model = "claude-3-5-sonnet-20241022",
+            temperature = 0,
+            max_tokens = 4096,
+        },
+    }
+)
