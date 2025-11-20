ba9c4349dfc5c4c2cb55853721fb0f85ac10405b
thesandybridge <mattmillerf@pm.me>
2025-11-20 14:53:58 -0500

    stuff

--- lua/thesbx/set.lua
+++ lua/thesbx/set.lua
@@ -1,6 +1,14 @@
 vim.opt.laststatus = 3 -- global statusline for all buffers
 vim.opt.winbar = "%=%m %f"
 vim.opt.guicursor = ""
+--vim.opt.guicursor = {
+--    "n-v-c:block",
+--    "i-ci-ve:ver25",
+--    "r-cr:hor20",
+--    "o:hor50",
+--    "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
+--    "sm:block-blinkwait175-blinkoff150-blinkon175"
+--}
 vim.opt.splitbelow = true
 vim.o.termguicolors = true
 
